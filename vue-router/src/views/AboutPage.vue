bc7196e439536651d47771cc855dea7efde2fdd4
Ingrid G <ingrid.gallegos@tipti.market>
2025-03-31 11:57:27 -0500

    feat: vue router set up

---
+++ vue-router/src/views/AboutPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Acerca de</h1>
+        <p>Esta es la página "Acerca de".</p>
+    </div>
+</template>
+<style scoped>
+
+</style>
---
+++ vue-router/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Inicio</h1>
+        <p>Bienvenido a la página principal.</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
