0d27c3da8faa4edc358a1773331fc463f0ba8f54
Ingrid G <ingrid.gallegos@tipti.market>
2025-03-31 16:30:48 -0500

    feat: create web history vs create webhash history

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -5,9 +5,9 @@
     <nav class="nav">
         <router-link to="/">Inicio</router-link>
         <router-link to="/brazil">Brazil</router-link>
+        <router-link to="/panama">Panama</router-link>
         <router-link to="/jamaica">Jamaica</router-link>
         <router-link to="/hawaii">Hawaii</router-link>
-    
     </nav>
     <div class="container">
         <router-view />
--- vue-router/src/router/index.ts
+++ vue-router/src/router/index.ts
@@ -4,6 +4,7 @@ import AboutPage from '@/views/AboutPage.vue';
 import Brazil from '../views/Brazil.vue';
 import Hawaii from '../views/Hawaii.vue';
 import Jamaica from '../views/Jamaica.vue';
+import Panama from '../views/Panama.vue';
 
 const routes = [
     { path: '/', component: HomePage },
@@ -11,12 +12,23 @@ const routes = [
     { path: '/brazil', component: Brazil },
     { path: '/hawaii', component: Hawaii },
     { path: '/jamaica', component: Jamaica },
-
+    { path: '/panama', component: Panama },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 });
+//2.9 create web history:
+/*urls que luce normal como '/brazil'
+* */
+
+//create web hash history
+/*crea hash mode '/#/brazil'
+practica comun en SPA para imitar el path sin hacer configuraciones especiales del servidor
+el trafico tiene un solo punto de entrada
+ */
 
 export default router;
+
+
